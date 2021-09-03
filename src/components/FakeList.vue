e99ea014d97aab2787ac48b19038bc2ec0206fb6
0x-jerry <x.jerry.wang@gmail.com>
2021-09-03 10:48:13 +0800

    perf: avoid flush when scroll up

--- src/components/FakeList.vue
+++ src/components/FakeList.vue
@@ -148,7 +148,7 @@ async function updateActiveItems() {
 
   const offscreenSize = 1000;
 
-  const top = window.scrollY - offscreenSize;
+  const top = window.scrollY;
   const maxTop = top + window.innerHeight + offscreenSize;
 
   let preTop = parentTop;
@@ -187,35 +187,6 @@ function observeItemHeightChanged() {
   elNodes.forEach((node) => {
     resizeObserver.observe(node);
   });
-
-  // resizeObserver.observe(document.querySelector('.data-'));
-
-  // // Select the node that will be observed for mutations
-  // const targetNode = document.getElementById("some-id");
-
-  // // Options for the observer (which mutations to observe)
-  // const config = { attributes: true, childList: true, subtree: true };
-
-  // // Callback function to execute when mutations are observed
-
-  // // Create an observer instance linked to the callback function
-  // const observer = new MutationObserver((mutationsList, observer) => {
-  //   for (const mutation of mutationsList) {
-  //     if (mutation.type === "childList") {
-  //       console.log("A child node has been added or removed.");
-  //     } else if (mutation.type === "attributes") {
-  //       console.log(
-  //         "The " + mutation.attributeName + " attribute was modified."
-  //       );
-  //     }
-  //   }
-  // });
-
-  // // Start observing the target node for configured mutations
-  // observer.observe(targetNode, config);
-
-  // // Later, you can stop observing
-  // observer.disconnect();
 }
 
 watch(() => [renderedItems.value], observeItemHeightChanged);
